56b7462632fd753f23cddd59597f58ef1547d0ef
Shubham Dubey <shubhamdubey671@gmail.com>
2023-03-06 18:51:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Loader from "./Loader";
+import { MathJax } from "better-react-mathjax";
+import "./style.scss";
+import "./loader.scss";
+import "./mediaQuery.scss";
 
 function App() {
+  const id = [
+    "AreaUnderTheCurve_901",
+    "BinomialTheorem_901",
+    "DifferentialCalculus2_901",
+  ];
+  const [question, setQuestion] = useState([]);
+  const [nextq, setNextq] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const server = `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=`;
+
+  const changeQuestion = (nextq) => {
+    setNextq(nextq);
+    setLoading(true);
+
+    if (nextq < 0) {
+      setNextq(2);
+    }
+    if (nextq > 2) {
+      setNextq(0);
+    }
+  };
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch(`${server}${id[nextq]}`);
+        const resJson = await response.json(); 
+        setQuestion(resJson[0]);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
+      };
+    fetchQuestions();
+  }, [server, id, nextq]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="full">
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="section">
+            <h1>{question.ChapterID}</h1>
+            <p>
+              <MathJax>{question.Question}</MathJax>
+            </p>
+            
+            <div className="buttons">
+            <button
+              className="button-9"
+              role="button"
+              onClick={() => changeQuestion(nextq - 1)}
+            >
+              {" "}
+              Prev
+            </button>
+            <button
+              className="button-9"
+              role="button"
+              onClick={() => changeQuestion(nextq + 1)}
+            >
+              {" "}
+              Next
+            </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
 export default App;
